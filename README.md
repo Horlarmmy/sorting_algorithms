2e232432642290f127e7763ebbf599cc537967de
Alade Toheeb <aladetoheeb5@gmail.com>
2022-07-19 20:42:23 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Sorting Algorithms Implemented with C
---
+++ sort.h
@@ -0,0 +1,7 @@
+#ifndef SORT_H_
+#define SORT_H_
+
+void print_list(const listint_t *list);
+void print_array(const int *array size_t size);
+
+#endif
